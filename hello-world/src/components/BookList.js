8ae6f488a7d2a7c52de281a80e2833aac35c5188
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-01-22 22:50:12 +0100

    added children prop to bookList component

--- hello-world/src/components/BookList.js
+++ hello-world/src/components/BookList.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 function BookList() {
-  const Image = (props) => (
-   <img src={props.image} alt="" />
-  );
+  const Image = (props) => <img src={props.image} alt="" />;
   
   const Title = (props) => {
     return <h2>{props.bookTitle}</h2>;
@@ -15,9 +13,10 @@ function BookList() {
   const Book = (props) => {
     return (
       <article className="book">
-        <Image image={props.image}/>
+        <Image image={props.image} />
         <Title bookTitle={props.bookTitle} />
         <Author authorName={props.authorName} />
+        <div>{props.children}</div>
       </article>
     );
   };
@@ -28,18 +27,27 @@ function BookList() {
         image="https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL600_SR600,400_.jpg"
         bookTitle="Atomic Habits"
         authorName="James Clear"
-      />
+      >
+        <p>First Paragraph</p>
+        <button>First Button</button>
+      </Book>
       <Book
         image="https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg"
         bookTitle="Fourth Wing"
         authorName="Rebecca Yarros"
-      />
+      >
+        <p>Second Paragraph</p>
+        <button>Second Button</button>
+      </Book>
       <Book
         image="https://images-na.ssl-images-amazon.com/images/I/917Bc9C1MlL._AC_UL600_SR600,400_.jpg"
         bookTitle="Iron Flame"
         authorName="Rebecca Yarros"
-      />
-      
+      >
+        <p>Third Paragraph</p>
+        <button>Third Button</button>
+      </Book>
+
       <Book
         image="https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL600_SR600,400_.jpg"
         bookTitle="Atomic Habits"
