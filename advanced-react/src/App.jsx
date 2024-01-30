a43a48ca54a9a123a512593db4f60523b9728bd4
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-01-31 00:32:01 +0100

    added project starter files

---
+++ advanced-react/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Starter from "./tutorial/01-useState/starter/01-error-example";
+import "./index.css";
+
+function App() {
+
+  return (
+    <div className="container">
+      <Starter />
+    </div>
+  );
+}
+
+export default App;
