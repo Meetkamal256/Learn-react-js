85ffe34f8c55ab7cd100442e497e8b0ea41a8337
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-01-31 23:38:07 +0100

    added a function to setState to update state

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Starter from "./tutorial/01-useState/starter/04-useState-object";
+import Starter from "./tutorial/01-useState/starter/05-useState-gotcha";
 import "./index.css";
 
 
