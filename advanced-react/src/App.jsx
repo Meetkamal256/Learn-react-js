53eea2292a76aa8b5e4a57a8f1a3e0581fc5f8d5
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-02-01 22:20:56 +0100

    useEffect hook example

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Starter from "./tutorial/02-useEffect/starter/02-useEffect-basics";
+import Starter from "./tutorial/02-useEffect/starter/03-multiple-effects";
 import "./index.css";
 
 
