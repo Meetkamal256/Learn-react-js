921416da919cb19b9a5201a2f219899f92e2c132
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-02-01 22:18:31 +0100

    useEffect hook example

--- advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const UseStateGotcha = () => {
   const [value, setValue] = useState(0);
-
+  
   const handleClick = () => {
     setTimeout(() => {
       setValue((currentState) => {
