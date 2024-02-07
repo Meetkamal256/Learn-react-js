559b3c181c9df623840a97ff76f176c1f5460ffe
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-02-07 01:29:43 +0100

    added starter files for tours project

---
+++ tours/src/Tour.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Tour() {
+  return <div>single Tour component</div>;
+}
+
+export default Tour;
---
+++ tours/src/Tours.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Tours() {
+  return (
+    <h1>Tours component</h1>
+  )
+}
+
+export default Tours
