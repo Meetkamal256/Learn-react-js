3bc75a3f17d3293254debb9f8f0e0b507ff45659
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-02-09 01:14:20 +0100

    added react-icons to project

---
+++ reviews/src/Review.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight, FaGithubSquare } from 'react-icons/fa';
+
+const Review = () => {
+  return <h2>review component
+    <FaChevronLeft />
+    <FaGithubSquare />
+  </h2>;
+};
+
+export default Review;
