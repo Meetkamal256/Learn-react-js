dc836549c98fc6ae1d043ad3e668cceb77786f49
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-02-11 04:00:35 +0100

    made the categories section dynamic

--- menu/src/App.jsx
+++ menu/src/App.jsx
@@ -4,15 +4,26 @@ import Categories from "./Categories";
 import items from "./data";
 import "./index.css";
 
+
+const allCategories = ['all',... new Set(items.map((item) => item.category))]  ;
+console.log(allCategories);
+
+
+console.log(allCategories);
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
-
+  const [categories, setCategories] = useState(allCategories);
+  
   const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+       return;
+    }
     const newItems = items.filter((item) => item.category === category);
     setMenuItems(newItems);
+   
   };
-
+  
   return (
     <main>
       <section className="menu section">
@@ -20,7 +31,7 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories filterItems={filterItems} />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
