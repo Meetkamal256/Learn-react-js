bed09551afede32009360ba675efd160f2f69640
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-02-12 00:37:44 +0100

    added starter files for tabs project

---
+++ tabs/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './index.css'
+
+const url = "https://course-api.com/react-tabs-project";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
