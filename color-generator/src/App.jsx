824d971eb15d68c052cc62032e17c8aac682d448
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-02-14 03:14:41 +0100

    added the error class to input field

--- color-generator/src/App.jsx
+++ color-generator/src/App.jsx
@@ -1,10 +1,48 @@
-import { useState } from 'react'
-import './index.css'
+import { useState } from "react";
+import "./index.css";
+import SingleColor from "./SingleColor";
+import Values from "values.js";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <h3>text</h3>
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+         let colors = new Values(color).all(10);
+         console.log(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+ 
+  };
+  
+  return (
+    <>
+      <section className="container">
+        <h3>Color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025" className={`${error ? 'error': null}`}
+          />
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>List goes here</h4>
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
+
+
