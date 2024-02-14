2da8d10fb6ad71df9fe2a4b9565be30eaefbb411
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-02-15 00:13:32 +0100

    added a default value to the color generator

--- color-generator/src/App.jsx
+++ color-generator/src/App.jsx
@@ -6,7 +6,7 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#f15025").all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,7 +19,7 @@ function App() {
       console.log(error);
     }
   };
-
+  
   return (
     <>
       <section className="container">
--- color-generator/src/SingleColor.jsx
+++ color-generator/src/SingleColor.jsx
@@ -8,13 +8,20 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const hex = rgbToHex(...rgb);
   const hexValue = `#${hexColor}`;
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
   return (
     <article
       className={`color ${index > 10 && "color-light"}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
       onClick={() => {
         setAlert(true);
-        navigator.clipboard.writeText(hexValue); 
+        navigator.clipboard.writeText(hexValue);
       }}
     >
       <p className="percent-value">{weight}%</p>
