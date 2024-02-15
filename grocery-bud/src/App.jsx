f38ae3238877282104fd5b520f3067f1bf467df5
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-02-15 02:23:05 +0100

    added project starter files

---
+++ grocery-bud/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./index.css";
+import Alert from "./Alert";
+import List from "./List";
+
+function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input type="text" />
+          <button type="submit" className="submit-btn">
+            {" "}
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">Clear items</button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
