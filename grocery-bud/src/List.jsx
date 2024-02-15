205690c671185180751538b1c22372c9e213b1c5
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-02-15 04:32:14 +0100

    added the remove item function

--- grocery-bud/src/List.jsx
+++ grocery-bud/src/List.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
-const List = ({ items }) => {
+const List = ({ items, removeItem }) => {
   return (
     <div className="grocery-list">
       {items.map((item) => {
@@ -11,7 +11,11 @@ const List = ({ items }) => {
             <div className="btn-container">
               <button className="edit-btn" type="button">
                 <FaEdit />
-                <button className="delete-btn" type="button">
+                <button
+                  className="delete-btn"
+                  type="button"
+                  onClick={() => removeItem(id)}
+                >
                   <FaTrash />
                 </button>
               </button>
