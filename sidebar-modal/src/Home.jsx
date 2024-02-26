69466ade867c9e6734c4fbb08d08b02c9c386717
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-02-26 08:25:49 +0100

    page structure done

---
+++ sidebar-modal/src/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+
+const Home = () => {
+  return (
+    <main>
+      <button className="sidebar-toggle">
+        <FaBars />
+          </button>
+          <button className="btn">
+              show modal
+          </button>
+    </main>
+  );
+};
+
+export default Home;
