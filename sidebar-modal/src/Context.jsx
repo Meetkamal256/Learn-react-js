96270af67cb6f9b23268466c4347f704a392a27a
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-02-27 03:58:24 +0100

    added state values for sidebar and modal

--- sidebar-modal/src/Context.jsx
+++ sidebar-modal/src/Context.jsx
@@ -3,13 +3,29 @@ import React, { useState, useContext } from "react";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeModal = () => {
+    setIsSidebarOpen(false);
+  };
+
   return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
 };
 
 // custom hook
 export const useGlobalContext = () => {
-    return useContext(AppContext);
-    
+  return useContext(AppContext);
 };
 
 export { AppContext, AppProvider };
