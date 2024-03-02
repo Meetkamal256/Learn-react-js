dd33cac07284646bc95b805dfa98ce2d27b8127c
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-03-02 08:14:07 +0100

    added starter  files for cart project

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -5,9 +5,15 @@ import reducer from "./reducer";
 const url = "https://course-api.com/react-useReducer-cart-project";
 const AppContext = React.createContext();
 
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
+
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer();
-  console.log(cart);
 
   return (
     <AppContext.Provider
