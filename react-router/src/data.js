38d7a4019f0b6b7b94ad8c9c9a2bcdd357ece08d
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-03-05 03:12:33 +0100

    form layout and controlled input setup

--- react-router/src/data.js
+++ react-router/src/data.js
@@ -1,36 +1,36 @@
 const products = [
   {
-    id: 'recZkNf2kwmdBcqd0',
-    name: 'accent chair',
+    id: "recZkNf2kwmdBcqd0",
+    name: "accent chair",
     image:
-      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681749482/react-comfy-store-products/iuYyO9RP_o_upinxq.jpg',
+      "https://images.unsplash.com/photo-1449247709967-d4461a6a6103?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    id: 'recEHmzvupvT8ZONH',
-    name: 'albany sectional',
+    id: "recEHmzvupvT8ZONH",
+    name: "albany sectional",
     
     image:
-      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681750782/react-comfy-store-products/product-2_lusrzx.jpg',
+      "https://images.unsplash.com/photo-1567016432779-094069958ea5?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    id: 'rec5NBwZ5zCD9nfF0',
-    name: 'albany table',
-    
+    id: "rec5NBwZ5zCD9nfF0",
+    name: "albany table",
+
     image:
-      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681750874/react-comfy-store-products/product-3_znpiqa.jpg',
+      "https://images.unsplash.com/photo-1491336440196-6d4fee45a05e?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    id: 'recd1jIVIEChmiwhe',
-    name: 'armchair',
-    
+    id: "recd1jIVIEChmiwhe",
+    name: "armchair",
+
     image:
-      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681750929/react-comfy-store-products/product-4_ebl6q1.jpg',
+      "https://images.unsplash.com/photo-1601366533287-5ee4c763ae4e?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    id: 'recoM2MyHJGHLVi5l',
-    name: 'bar stool',
+    id: "recoM2MyHJGHLVi5l",
+    name: "bar stool",
     image:
-      'https://res.cloudinary.com/dt2g7mgtv/image/upload/v1681751026/react-comfy-store-products/product-5_n184nu.jpg',
+      "https://plus.unsplash.com/premium_photo-1674326713405-06b44b439013?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
