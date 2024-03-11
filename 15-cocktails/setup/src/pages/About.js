0f3aef2ef89ac750481241fb9bf5c97f72e75c78
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-03-11 17:18:06 +0100

    about and error layout and styling done

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section class='section about-section'>
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, sequi
+        soluta. Aliquam velit dicta dolorum totam enim deserunt aperiam quasi
+        nesciunt, excepturi nobis vel culpa quibusdam unde fugiat obcaecati.
+        Quisquam sapiente dignissimos pariatur excepturi rem libero repudiandae
+        quasi, at in?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
