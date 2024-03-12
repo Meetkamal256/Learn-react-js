b6f1422488f92fd276487fd0400a2a6d85772b70
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-03-12 14:52:19 +0100

    fetched drink api

--- 15-cocktails/setup/src/components/CocktailList.js
+++ 15-cocktails/setup/src/components/CocktailList.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import Cocktail from './Cocktail'
-import Loading from './Loading'
-import { useGlobalContext } from '../context'
+import React from "react";
+import Cocktail from "./Cocktail";
+import Loading from "./Loading";
+import { useGlobalContext } from "../context";
 
 const CocktailList = () => {
+  const { loading, cocktails } = useGlobalContext();
+  console.log(cocktails);
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (cocktails.length < 1) {
+    return <h2 className="section-title">No cocktails match your criteria</h2>;
+  }
   return (
     <div>
-      <h2>cocktail list component</h2>
+      <section>
+        <h2 className="section-title">Cocktails</h2>
+        <div className="cocktails-center">
+          {cocktails.map((item) => {
+            return <Cocktail key={item.id} {...item} />;
+          })}
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default CocktailList
+export default CocktailList;
